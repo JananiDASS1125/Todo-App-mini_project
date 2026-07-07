a7babe1029b3a14e97a810377ff9693f0f8f2d38
JananiDASS1125 <jananidass45@gmail.com>
2026-07-07 17:13:12 +0530

    Middleware created

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app=express();
 connectDB();
 app.use(cors());
 app.use(express.json());
+const userRoutes=require("./routes/userRoutes");
+app.use("/api/users".userRoutes);
 
 app.get("/" ,(req,res)=>{
     res.send("API Running");
